a1199d96a423395ae8e8be883c242c70ae3ff919
zeegeek <zeegeek>
2007-05-07 04:57:04 +0000

    Made program name capitalized.

--- constants.py
+++ constants.py
@@ -16,7 +16,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-NAME = 'pymailheaders'
+NAME = 'Pymailheaders'
 VERSION = 'Beta 0.4'
 HOMEPAGE = 'http://pymailheaders.sourceforge.net'
 AUTHOR = 'Neil Shi'
