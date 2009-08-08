fe59f1cbfb4d7c15c2f1c822b4d823f57f02db55
Ning Shi <zeegeek@gmail.com>
2009-08-08 15:09:15 -0400

    Refactor POP3 protocol module.

--- popprl.py
+++ popprl.py
@@ -25,7 +25,6 @@ from email.utils import parseaddr, parsedate_tz, mktime_tz
 from email.Header import decode_header
 from datetime import datetime
 
-import chardet
 from exception import *
 
 class pop:
@@ -68,8 +67,36 @@ class pop:
         self.__pass = password
         self.__ssl = ssl
         self.__size = h
+        self.__connection = None
         self.__message_dict = []
         self.__logger = logging.getLogger('pop')
+
+    def __command(self, cmd, method_name, *args, **kwargs):
+        """Run the POP3 command.
+
+        @type cmd: string
+        @param cmd: the POP3 command to run
+        @type method_name: string
+        @param method_name: the name of the method which called this method.
+        @rtype: list
+        @return: response from the POP3 command
+        """
+
+        try:
+            response = getattr(self.__connection, cmd)(*args, **kwargs)
+            if response[0:3] != '+OK':
+                self.__logger.error(response[4:])
+                raise Error('popprl (%s)' % method_name, response[4:])
+        except socket.gaierror, (socket.EAI_AGAIN, strerr):
+            self.__logger.error(str(strerr))
+            raise TryAgain('popprl (%s)' % method_name, strerr)
+        except (socket.error, socket.gaierror, poplib.error_proto), strerr:
+            self.__logger.error(str(strerr))
+            self.__disconnect()
+            raise Error('popprl (%s)' % method_name, strerr)
+        except:
+            self.__disconnect()
+            raise
 
     def __disconnect(self):
         """Destructor
@@ -77,6 +104,9 @@ class pop:
         """
 
         self.__logger.debug('Destroy')
+
+        if not self.__connection:
+            return
 
         try:
             response = self.__connection.quit()
@@ -89,6 +119,8 @@ class pop:
             raise Error('popprl (__disconnect)', strerr)
         except:
             raise
+        finally:
+            self.__connection = None
 
     def __connect(self):
         """Connect to the server and log in.
@@ -103,6 +135,9 @@ class pop:
 
         self.__logger.debug('Connect')
 
+        if self.__connection:
+            return
+
         try:
             if self.__ssl:
                 self.__connection = poplib.POP3_SSL(self.__server)
@@ -111,34 +146,18 @@ class pop:
 
             # Authentication: send username and password in clear
             # text.
-            response = self.__connection.user(self.__uname)
-            if response[0:3] != '+OK':
-                self.__logger.error('Failed to login with username %s',
-                                    self.__uname)
-                raise Error('popprl (connect)', _('Login failed'))
-            response = self.__connection.pass_(self.__pass)
-            if response[0:3] != '+OK':
-                self.__logger.error('Failed to login with password %s',
-                                    self.__pass)
-                raise Error('popprl (connect)', _('Login failed'))
+            self.__command('user', '__connect', self.__uname)
+            self.__command('pass_', '__connect', self.__pass)
         except socket.gaierror, (socket.EAI_AGAIN, strerr):
             self.__logger.error(str(strerr))
-            raise TryAgain('popprl (connect)', strerr)
+            raise TryAgain('popprl (__connect)', strerr)
         except (socket.error, socket.gaierror, poplib.error_proto), strerr:
             self.__logger.error(str(strerr))
-            raise Error('popprl (connect)', strerr)
+            self.__disconnect()
+            raise Error('popprl (__connect)', strerr)
         except:
+            self.__disconnect()
             raise
-
-    def connect(self):
-        """Dummy function for compatibility reason.
-
-        Since POP3 locks the maildrop once the user has been
-        authenticated, we don't connect unless we're checking for new
-        messages.
-        """
-
-        pass
 
     def get_mail(self):
         """Get mails.
@@ -162,82 +181,68 @@ class pop:
 
         self.__logger.debug('Get mail')
 
-        try:
-            # 1. connect to the server
-            self.__connect()
+        # 1. connect to the server
+        self.__connect()
 
-            # 2. get the total number of messages in the mailbox.
-            # POP3 has no way of indicating how many new messages
-            # there are.
-            total = self.__connection.stat()[0]
+        # 2. get the total number of messages in the mailbox.
+        # POP3 has no way of indicating how many new messages
+        # there are.
+        total = self.__command('stat', 'get_mail')[0]
 
-            message_range = range(total, 0, -1)
-            for i in message_range:
-                # if the number of messages reaches the displayable max, stop
-                if (len(messages[0]) + len(messages[1])) == self.__size:
-                    break
+        message_range = range(total, 0, -1)
+        for i in message_range:
+            # if the number of messages reaches the displayable max, stop
+            if (len(messages[0]) + len(messages[1])) == self.__size:
+                break
 
-                # 3. get unique IDs
-                response = self.__connection.uidl(i)
-                if response[0:3] != '+OK':
-                    self.__logger.error('Failed to fetch message ID')
-                    raise Error('popprl (get_mail)',
-                                _('Fetching message ID failed'))
-                uid = re.search('([\S]*)$', response).group(1)
+            # 3. get unique IDs
+            response = self.__command('uidl', 'get_mail', i)
+            uid = re.search('([\S]*)$', response).group(1)
 
-                # 4. get message hearders
-                response = self.__connection.top(i, 0)
-                if response[0][0:3] != '+OK':
-                    self.__logger.error('Failed to fetch message')
-                    raise Error('popprl (get_mail)',
-                                _('Fetching messages failed'))
-
-                def d(x):
-                    # In case the string is not compliant with the standard,
-                    # let's make it correct.
-                    try:
-                        y = decode_header(re.sub(r'(=\?([^\?]*\?){3}=)',
-                                                 r' \1 ', x))
-                        res = ''.join(s[1] and s[0].decode(s[1]) or
-                                      s[0] for s in y)
-                        # Strips multiple contiguous white spaces into one.
-                        res = ' '.join(res.split())
-                        return res
-                    except UnicodeDecodeError:
-                        self.__logger.error('Invalid encoding')
-                        raise Error('popprl (get_mail)', _('Invalid encoding'))
-
-                def b(x):
-                    r = re.search('^(From|Subject|Date)', x)
-                    if r == None:
-                        return False
-                    else:
-                        return True
-
-                filtered_header = filter(b, response[1])
-                msg = message_from_string('\r\n'.join(filtered_header))
-                subject = d(msg['subject'])
-                (sender, addr) = parseaddr(msg['from'])
-                sender = sender and d(sender) or addr
-                date = parsedate_tz(msg['date'])
-                dt = date and datetime.fromtimestamp(mktime_tz(date)) or \
-                    datetime.now()
-
-                if uid in self.__message_dict:
-                    # old message
-                    messages[1].append((dt, sender, subject))
-                else:
-                    # new message
-                    messages[0].append((dt, sender, subject))
-                    self.__message_dict.append(uid)
+            # 4. get message hearders
+            response = self.__command('top', 'get_mail', i, 0)
 
             # 5. disconnect from server. Don't keep POP3 server
             # locking up the mailbox.
             self.__disconnect()
-        except (socket.error, socket.gaierror, poplib.error_proto,), strerr:
-            self.__logger.error(str(strerr))
-            raise Error('popprl (get_mail)', strerr)
-        except:
-            raise
+
+            def d(x):
+                # In case the string is not compliant with the standard,
+                # let's make it correct.
+                try:
+                    y = decode_header(re.sub(r'(=\?([^\?]*\?){3}=)',
+                                             r' \1 ', x))
+                    res = ''.join(s[1] and s[0].decode(s[1]) or
+                                  s[0] for s in y)
+                    # Strips multiple contiguous white spaces into one.
+                    res = ' '.join(res.split())
+                    return res
+                except UnicodeDecodeError:
+                    self.__logger.error('Invalid encoding')
+                    raise Error('popprl (get_mail)', _('Invalid encoding'))
+
+            def b(x):
+                r = re.search('^(From|Subject|Date)', x)
+                if r == None:
+                    return False
+                else:
+                    return True
+
+            filtered_header = filter(b, response[1])
+            msg = message_from_string('\r\n'.join(filtered_header))
+            subject = d(msg['subject'])
+            (sender, addr) = parseaddr(msg['from'])
+            sender = sender and d(sender) or addr
+            date = parsedate_tz(msg['date'])
+            dt = date and datetime.fromtimestamp(mktime_tz(date)) or \
+                datetime.now()
+
+            if uid in self.__message_dict:
+                # old message
+                messages[1].append((dt, sender, subject))
+            else:
+                # new message
+                messages[0].append((dt, sender, subject))
+                self.__message_dict.append(uid)
 
         return messages
